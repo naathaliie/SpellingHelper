c31a11ec9e0649b7f8ce4b9a8fd1fae79e232fec
naathaliie <nathalielarsson@live.se>
2024-10-05 16:53:57 +0200

    fonts fix

--- spellingHelper/src/App.vue
+++ spellingHelper/src/App.vue
@@ -1,9 +1,22 @@
 <script setup></script>
 
 <template>
-  <div class="text-3xl font-bold text-pink-500 font-Handwritten">
-    Hello World!
+  <div class="text-pink-500 handWritten m-4">
+    Jag skrivs i handwritten-fonten
   </div>
+
+  <div class="text-orange-400 comicNeue-regular">
+    Jag skrivs i comicNeue-fonten
+  </div>
+
+  <div class="text-orange-400 comicNeue-bold">
+    Jag skrivs i comicNeue-Bold-fonten
+  </div>
+
+  <div class="text-blue-400 comicNeue-light">
+    Jag skrivs med comicNeue-Light-fonten
+  </div>
+  <div>jag skrivs med fonten som är default</div>
 </template>
 
 <style scoped></style>
--- spellingHelper/tailwind.config.js
+++ spellingHelper/tailwind.config.js
@@ -1,18 +1,65 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily: {
-        Platform: ['Platform', 'sans-serif'],
-        'Reenie-Beanie': ['Reenie Beanie', 'cursive'],
-        'Handwritten': ['The Girl Next Door', 'cursive'], // Lägg till din handskrift font här
+      fontFamily: { // Lägg till din handskrift font här
+        Platform: ["Platform", "sans-serif"],
+        handWritten: ["The Girl Next Door", "cursive"],
+        comicNeue: ["ComicNeue"], 
+      },
+      fontSize: {
+        "title-xl": [
+          "5.625rem",
+          { lineHeight: "6.563rem", fontWeight: "bold" },
+        ],
+        "title-xl-mobile": [
+          "3.125rem",
+          { lineHeight: "3.75rem", fontWeight: "bold" },
+        ],
+        "title-lg": [
+          "4.375rem",
+          { lineHeight: "4.063rem", fontWeight: "bold" },
+        ],
+        "title-lg-mobile": [
+          "2.5rem",
+          { lineHeight: "3.125rem", fontWeight: "bold" },
+        ],
+        "title-md": [
+          "3.438rem",
+          { lineHeight: "3.125rem", fontWeight: "bold" },
+        ],
+        "title-md-mobile": [
+          "2.5rem",
+          { lineHeight: "3.125rem", fontWeight: "bold" },
+        ],
+        "title-sm": [
+          "2.188rem",
+          { lineHeight: "2.813rem", fontWeight: "bold" },
+        ],
+        "title-xs": ["1.25rem", { lineHeight: "1.875rem", fontWeight: "bold" }],
+        headline: ["3.75rem", { lineHeight: "2.5rem" }],
+        "headline-mobile": ["2.5rem", { lineHeight: "2.5rem" }],
+        "body-lg": ["1.5rem", { lineHeight: "2.188rem" }],
+        "body-lg-mobile": ["1.25rem", { lineHeight: "1.875rem" }],
+        "body-md": ["1.25rem", { lineHeight: "1.875rem" }],
+        "body-sm": ["1rem", { lineHeight: "1.563rem" }],
+        "body-xs": ["0.875rem", { lineHeight: "1.25rem" }],
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '2rem',
+        xl: '1rem',
+      },
+      screens: {
+        DEFAULT: '100%',
+        sm: '100%',
+        md: '100%',
+        lg: '1440px',
       },
     },
   },
   plugins: [],
-}
-
+};
